ac8f57994228fbef1d1c5e30dc79b8e37b53d3d2
ayushsarojportfolio <89305532+ayushsarojportfolio@users.noreply.github.com>
2021-10-20 18:35:14 +0530

    Update participation.js

--- assets/js/participation.js
+++ assets/js/participation.js
@@ -72,7 +72,7 @@ const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
     title: "PICTURE WITH WHOLE GROUP",
-    cardImage: "assets/images/experience-page/WhatsApp Image 2021-01-26 at 3.46.55 PM.JPG",
+    cardImage: "https://github.com/ayushsarojportfolio/ayushsarojportfolio.github.io/blob/main/assets/images/experience-page/WhatsApp%20Image%202021-01-26%20at%203.46.55%20PM.jpg",
     description:
       "WHOLE GROUP WORK HARD FOR REPUBLIC DAY CELEBRATION",
   },
@@ -90,9 +90,9 @@ const volunteershipcards = [
   },
   {
     title: "HAVING FUN AFTER CELEBRATION",
-    cardImage: "assets/images/experience-page/WhatsApp Image 2021-01-26 at 1.18.08 PM.JPEG",
+    cardImage: "https://github.com/ayushsarojportfolio/ayushsarojportfolio.github.io/blob/main/assets/images/experience-page/WhatsApp%20Image%202021-01-26%20at%201.18.08%20PM.jpeg",
     description:
-      "Moments...............",
+      "",
   },
  
 ];
